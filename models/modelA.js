bd1e2a59964409fd0eab1cc28fc061748f05c204
Dillon Parker <prkr93@gmail.com>
2021-09-24 17:55:14 -0600

    Add dinosaur object

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  name: "Roger",
+  breed: "Triceratops",
+  age: 55,
+  numOfLegs: 4,
+  enjoysMeat: false,
+  enjoysPlants: true,
+  siblingsAges: [63, 51, 66],
+  siblingsNames: ["Linda", "Lisa", "Margo"]
+}
