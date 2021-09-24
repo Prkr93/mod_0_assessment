d8c8b64b77344e13a4c59a4c5e3f716d9a177f07
Dillon Parker <prkr93@gmail.com>
2021-09-24 17:43:14 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var movie = "Spring";
+var isEntertaining = true;
+movie = "Parent Trap";
+var num4 = 6;
+var num6 = 11;
+var product = num4 * num6;
+var favoriteFoods = ["Cannoli", "Chicken Tenders", "French Fries"];
+favoriteFoods.pop();
+console.log(movie.length);
