5af8b4e97f39a3dee4a8a8ac77e2a5b42daa57a7
Dillon Parker <prkr93@gmail.com>
2021-09-24 18:04:50 -0600

    Add developer object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  name: "Dillon",
+  program: "Front-end",
+  age: 28,
+  numOfPets: 1,
+  caffeinated: false,
+  hungry: true,
+  hobbies: ["knitting", "video games", "hiking", "rock climbing"],
+  favBands: ["Lord Huron", "Givers", "Mumford and Sons"]
+}
